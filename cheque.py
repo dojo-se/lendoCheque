1fd9b7ceb06de741b5456ec0ec8552a98d32e3ce
GDG Aracaju <gdgaracaju@gmail.com>
2016-04-27 21:53:52 -0300

    Refatoraca com tres partes

--- cheque.py
+++ cheque.py
@@ -3,15 +3,21 @@ inteiros = {
 	"dois": 2,
 	"tres": 3,
 	"quatro": 4,
+	"cinco": 5,
 	"treze": 13,
 	"quinze": 15,
-	"vinte": 20
+	"vinte": 20,
+	"cento": 100,
+	"cem": 100
 }
 
 def ler_cheque(extenso):
 
 	partes = extenso.split()
-	if partes[1] == "e":
+
+	if len(partes) > 2 and partes[3] == "e":
+		valor = ler_numero(partes[0]) + ler_numero(partes[2]) + ler_numero(partes[4])
+	elif partes[1] == "e":
 		valor = ler_numero(partes[0]) + ler_numero(partes[2])
 	else:
 		valor = ler_numero(partes[0])
--- test_cheque.py
+++ test_cheque.py
@@ -18,7 +18,25 @@ class TestCheque(unittest.TestCase):
 	def test_21(self):
 		self.assertEqual(cheque.ler_cheque('vinte e um reais'), 21)
 
+	def test_22(self):
+		self.assertEqual(cheque.ler_cheque('vinte e dois reais'), 22)
+
+	def test_23(self):
+		self.assertEqual(cheque.ler_cheque('vinte e tres reais'), 23)
+
+	def test_24(self):
+		self.assertEqual(cheque.ler_cheque('vinte e quatro reais'), 24)
+
+	def test_120(self):
+		self.assertEqual(cheque.ler_cheque('cento e vinte reais'), 120)
+
+	def test_100(self):
+		self.assertEqual(cheque.ler_cheque('cem reais'), 100)
+
+	def test_125(self):
+		self.assertEqual(cheque.ler_cheque('cento e vinte e cinco reais'), 125)		
+
 	# ['vinte', 'e', 'um']
-	
+
 	#def test_1_1(self):
 	#	self.assertEqual(cheque.ler_cheque('um real e um centavo'), 1.1)
