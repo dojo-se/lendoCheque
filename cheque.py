eab51ad6ff3daff27bec045ec19f679bc62bef98
GDG Aracaju <gdgaracaju@gmail.com>
2016-04-27 21:39:51 -0300

    Refarotação para dicionario (Eduardo e Jessica)

--- cheque.py
+++ cheque.py
@@ -1,13 +1,23 @@
+inteiros = {
+	"um": 1,
+	"dois": 2,
+	"tres": 3,
+	"quatro": 4,
+	"treze": 13,
+	"quinze": 15,
+	"vinte": 20
+}
+
 def ler_cheque(extenso):
 
 	partes = extenso.split()
+	if partes[1] == "e":
+		valor = ler_numero(partes[0]) + ler_numero(partes[2])
+	else:
+		valor = ler_numero(partes[0])
 
-	if partes[0] == "um":
-		return 1
+	return valor
 
-	if partes[0] == "dois":
-		return 2
 
-	#if extenso == "um real e um centavo":
-	#	return 1.1
-
+def ler_numero(numeroStr):
+	return inteiros[numeroStr]
--- test_cheque.py
+++ test_cheque.py
@@ -5,7 +5,20 @@ import cheque
 class TestCheque(unittest.TestCase):
 	def test_1(self):
 		self.assertEqual(cheque.ler_cheque('um real'), 1)
+
 	def test_2(self):
 		self.assertEqual(cheque.ler_cheque('dois reais'), 2)
+
+	def test_13(self):
+		self.assertEqual(cheque.ler_cheque('treze reais'), 13)
+
+	def test_15(self):
+		self.assertEqual(cheque.ler_cheque('quinze reais'), 15)
+		
+	def test_21(self):
+		self.assertEqual(cheque.ler_cheque('vinte e um reais'), 21)
+
+	# ['vinte', 'e', 'um']
+	
 	#def test_1_1(self):
 	#	self.assertEqual(cheque.ler_cheque('um real e um centavo'), 1.1)
